7a6ebe639738fc458f794befe75f8ab00e3a0f45
Lester Tan <127372303+SunsetStories@users.noreply.github.com>
2023-11-22 16:50:43 +0800

    Add files via upload

---
+++ Chapter 2_The_Spring_Context_Defining_beans/2.2.1 adding beans of same type to Spring context/demo/src/main/java/Parrot/Parrot.java
@@ -0,0 +1,12 @@
+package Parrot;
+public class Parrot {
+    private String name;
+
+    public void setName(String n) {
+        this.name = n;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+}
