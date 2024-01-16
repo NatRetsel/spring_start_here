89e5d75708d16337fd85ace87f1281a253bb8461
Lester Tan <127372303+NatRetsel@users.noreply.github.com>
2024-01-16 14:43:17 +0800

    Add files via upload

---
+++ Chapter 10_Implementing_REST_services/Ch10-ex6/demo/src/main/java/com/example/demo/model/PaymentDetails.java
@@ -0,0 +1,13 @@
+package com.example.demo.model;
+
+public class PaymentDetails {
+    private double amount;
+
+    public void setAmount(double amount){
+        this.amount = amount;
+    }
+
+    public double getAmount(double amount) {
+        return this.amount;
+    }
+}
