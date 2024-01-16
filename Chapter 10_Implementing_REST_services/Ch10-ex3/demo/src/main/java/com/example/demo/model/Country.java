7e6397591b90f443b9fc6652d0c995a6f23ba8fa
Lester Tan <127372303+NatRetsel@users.noreply.github.com>
2024-01-16 14:41:42 +0800

    Add files via upload

---
+++ Chapter 10_Implementing_REST_services/Ch10-ex3/demo/src/main/java/com/example/demo/model/Country.java
@@ -0,0 +1,28 @@
+package com.example.demo.model;
+
+public class Country {
+    private String name; 
+	private int population; 
+	public static Country of( String name, int population) { 
+		Country country = new Country(); 
+		country.setName(name); 
+		country.setPopulation(population); 
+		return country; 
+	} 
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPopulation(int population) {
+        this.population = population;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getPopulation() {
+        return this.population;
+    }
+}
